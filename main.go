2f6f97ecfe035b2878ee0d7e551ee6440ce39ba0
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2024-07-13 09:15:29 +0700

    refactoring func

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 )
 
 func main() {
+	//get urls from categories
 	urlsGenres := core.ParseGenres()
 	urlsCountry := core.ParseCountry()
 	urlsYears := core.ParseYears()
 
+	// scraping data
 	core.ParseData(urlsGenres, "genres")
 	core.ParseData(urlsCountry, "country")
 	core.ParseData(urlsYears, "years")
